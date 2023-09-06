701005a2bd2e43ebc7b68ec22f87799b3e1a615a
Sagar Lakshmipathy <18vidhyasagar@gmail.com>
2023-09-05 17:33:23 -0700

    write to kafka not working

--- src/jobs/part4integrations/App1IntegratingKafka.py
+++ src/jobs/part4integrations/App1IntegratingKafka.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 
+from src.jobs.common.package import carsSchema
 from src.utils import config_loader
 
 spark = SparkSession.builder \
@@ -11,8 +12,10 @@ spark = SparkSession.builder \
     .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.1") \
     .getOrCreate()
 
+
 config = config_loader("/Users/sagarl/projects/spark-streaming-pyspark/src/config.json")
 dataPath = config["dataPath"]
+checkpointPath = config["checkpointPath"]
 
 def readFromKafka():
     kafkaDF = spark.readStream \
@@ -30,5 +33,27 @@ def readFromKafka():
         .awaitTermination()
 
 
+def writeToKafka():
+    carsDF = spark.readStream \
+        .format("json") \
+        .schema(carsSchema) \
+        .load(f"{dataPath}/cars")
+
+    carsKafkaDF = carsDF.select(
+        upper(col("Name")).alias("key"),
+        col("Name").alias("value")
+        )
+
+    carsKafkaDF.writeStream \
+        .format("kafka") \
+        .outputMode("append") \
+        .option("kafka.bootstrap.servers", "localhost:9092") \
+        .option("topic", "rockthejvm") \
+        .option("checkpointLocation", checkpointPath) \
+        .start() \
+        .awaitTermination()
+
+
 if __name__ == '__main__':
-    readFromKafka()
+    # readFromKafka()
+    writeToKafka()
