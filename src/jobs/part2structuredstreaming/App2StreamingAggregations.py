621b8ffd20aea7b5404f1a5b1cee83fa5ac70af2
Sagar Lakshmipathy <18vidhyasagar@gmail.com>
2023-09-05 18:26:43 -0700

    line count aggregation working

---
+++ src/jobs/part2structuredstreaming/App2StreamingAggregations.py
@@ -0,0 +1,33 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+from src.utils import config_loader
+
+spark = SparkSession.builder \
+    .appName("Streaming Aggregations") \
+    .master("local[2]") \
+    .getOrCreate()
+
+config = config_loader("/Users/sagarl/projects/spark-streaming-pyspark/src/config.json")
+dataPath = config["dataPath"]
+
+
+def streamingCount():
+    lines = spark.readStream \
+        .format("socket") \
+        .option("host", "localhost") \
+        .option("port", 12345) \
+        .load()
+
+    lineCount = lines.select(count(col("*")).alias("lineCount"))
+
+    # append output mode can only be used with watermarks
+    lineCount.writeStream \
+        .format("console") \
+        .outputMode("complete") \
+        .start() \
+        .awaitTermination()
+
+
+if __name__ == '__main__':
+    streamingCount()
