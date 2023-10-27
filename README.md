a9211abe1177b2085d9ec155b6d6e26e0f333d96
Sagar Lakshmipathy <18vidhyasagar@gmail.com>
2023-10-27 09:25:20 -0700

    Deploy website - based on 24e7a237e4ba6b73230e83f9a87ca60c85f1e393

--- README.md
+++
@@ -1,3 +0,0 @@
-# spark-streaming-pyspark
-
-This project is a PySpark equivalent of the Rock The JVM spark-streaming course.
--- src/utils.py
+++
@@ -1,7 +0,0 @@
-import json
-
-
-def config_loader(filepath) -> dict:
-    with open(filepath) as file:
-        config = json.load(file)
-        return config
